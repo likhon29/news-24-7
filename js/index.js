338d98c17e698b672b958f9975a2c25779c4f40b
Md Abdur Rouf Likhon <mdabdurrouf.likhon@gmail.com>
2022-09-03 20:22:20 +0600

    Category content added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.log("JS file working");
+const loadNews = () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displayCategories(data.data.news_category));
+};
+  
+const displayCategories = (categories) => {
+    console.log(categories);
+    let categoriesContainer = document.getElementById("categoriesContainer");
+    categories.forEach((category) => {
+      const span = document.createElement("span");
+      span.innerHTML = `
+          <span onclick="categoriesContent(${category.category_id})">${category.category_name}</span>
+          `;
+      categoriesContainer.appendChild(span);
+    });
+};
+const categoriesContent = (id) => {
+      console.log(id);
+  }
+
+loadNews();
