b82573db07ec42dc301f08edd67d2d8d76f603d8
Md Abdur Rouf Likhon <mdabdurrouf.likhon@gmail.com>
2022-09-03 20:50:30 +0600

    Category content added

--- js/index.js
+++ js/index.js
@@ -3,9 +3,8 @@ const loadNews = () => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => displayCategories(data.data.news_category));
-};
-  
-const displayCategories = (categories) => {
+  };
+  const displayCategories = (categories) => {
     console.log(categories);
     let categoriesContainer = document.getElementById("categoriesContainer");
     categories.forEach((category) => {
@@ -15,9 +14,82 @@ const displayCategories = (categories) => {
           `;
       categoriesContainer.appendChild(span);
     });
-};
-const categoriesContent = (id) => {
-      console.log(id);
+  };
+  
+  const categoriesContent = (id) => {
+    console.log(id);
+    const url = `https://openapi.programming-hero.com/api/news/category/${
+      "0" + id
+    }`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displayCategoriesContent(data.data));
+  };
+  
+  const displayCategoriesContent = (data) => {
+      let allNews = document.getElementById('allNews')
+      allNews.textContent = ''
+      if (data.length > 0) {
+          data.forEach(element => {
+              let div = document.createElement('div')
+              div.classList.add('p-5')
+              div.innerHTML = `
+              <div class="card mb-3 p-5">
+              <div class="row g-0">
+                  <div class="col-md-4">
+                      <img src="${element.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+                  </div>
+                  <div class="col-md-8">
+                      <div class="card-body">
+                          <h1 class="card-title">${element.title}</h1>
+                          <p class="card-text">${element.details.length > 350 ? element.details.slice(0, 200) + '...' : element.details}</p>
+                          <div class='d-flex justify-content-between align-items-center'>
+                          <div class='d-flex align-items-center'>
+                              <span class='me-3'><img src="${element.author.img}" style='width:50px ;border-radius:50%' alt="" /></span>
+                              <span> <h4>${element.author.name ? element.author.name : "No Name"}</h4></span>
+                          </div>
+                          <div>
+                          <i class="fa-solid fa-eye me-1"></i>${element.total_view ? element.total_view : "No Content"}
+                          </div>
+                          <div>
+                          <i class="fa-solid fa-star full"></i>
+                          <i class="fa-solid fa-star full"></i>
+                          <i class="fa-solid fa-star full"></i>
+                          <i class="fa-solid fa-star full"></i>
+                          <i class="fa-solid fa-star"></i>
+                          </div>
+                          <div onclick='showAllDetails("${element._id}")' data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-arrow-right" ></i></div>
+              </button>
+                          
+                          </div>
+                      </div>
+                  </div>
+                  
+              </div>
+          </div>
+              `
+           
+              allNews.appendChild(div)
+  
+          })
+      }
+      else {
+  
+          let div = document.createElement('div')
+          div.classList.add('p-5')
+          div.innerHTML = `
+              <div class="card mb-3 p-5">
+              <h1 class='text-center'>No Content</h1>
+              </div>
+              `
+       
+          allNews.appendChild(div)
+  
+  
+      }
   }
+  
+ 
 
-loadNews();
+  loadNews();
+  
